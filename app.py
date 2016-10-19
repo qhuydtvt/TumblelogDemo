121112e491b508df8265b73b65fe75798cadf0d0
qhuydtvt <qhuydtvt@gmail.com>
2016-10-19 20:46:55 +0700

    Added posts

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
+import json
 from flask import Flask
 
 app = Flask(__name__)
 
+post1 = {
+    "title" : "Good day",
+    "content": "Today I met a girl. She had black eyes and loves ice-cream"
+}
+
+post2 = {
+    "title": "Bad day",
+    "content": "Today, she did show up and I felt more lonely than ever"
+}
+
+print(post1["title"])
+print(post1["content"])
+
+posts = [post1, post2]
 
 @app.route('/')
-def hello_world():
-    return 'Hello TechKids-Android 5'
+def main():
+    return json.dumps(posts)
 
 
 if __name__ == '__main__':
